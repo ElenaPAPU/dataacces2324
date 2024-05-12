0e6e267737c5c51d88c31576a1d76419ada2a421
Elena <demo@example.com>
2024-05-12 22:13:22 +0300

    curs1

---
+++ curs1/comenzi.sql
@@ -0,0 +1,21 @@
+Expand Requery Edit Bookmark Database : demo2324 Queried time: 20:20:12
+INSERT INTO `contact`(`id`,`nume`,`prenume`,`telefon`,`email`,`mesaj`,`sex`,`data_adaugare`) VALUES(NULL)
+SELECT * FROM `contact`
+DELETE FROM `demo2324`.`contact`
+SELECT * FROM `contact`
+INSERT INTO `contact`(`id`,`nume`,`prenume`,`telefon`,`email`,`mesaj`,`sex`,`data_adaugare`) VALUES(NULL)
+SELECT * FROM `contact`
+TRUNCATE TABLE `demo2324`.`contact`
+SELECT * FROM `contact`
+INSERT INTO `contact`(`id`,`nume`,`prenume`,`telefon`,`email`,`mesaj`,`sex`,`data_adaugare`) VALUES(NULL)
+SELECT * FROM `contact`
+SELECT * FROM `contact`
+UPDATE `contact` SET `nume`=`dobre` WHERE `contact`.`id`=1;
+UPDATE contact SET nume =`dobre`, prenume=`elena` WHERE id=1;
+SELECT * FROM `contact`
+INSERT INTO `contact`(`id`,`nume`,`prenume`,`telefon`,`email`,`mesaj`,`sex`,`data_adaugare`) VALUES(NULL)
+SELECT * FROM `contact`
+-- DELETE FROM contact WHERE id=3;
+SELECT nume, prenume FROM `contact`;
+DELETE FROM contact WHERE id=3;
+SELECT * FROM `contact`
